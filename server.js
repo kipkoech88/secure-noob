88662f22d69181f7d87c9ef667d34f06f7530b9c
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-04 14:53:58 +0300

    added helmet middleware

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const PORT = process.env.PORT || 5000
+const helmet = require('helmet')
 
 // initializing app instance
 const app = express();
+
+//middlewares
+app.use(helmet());
 
 //basic API that returns 'hello world'
 app.get("/", (req,res)=>{
